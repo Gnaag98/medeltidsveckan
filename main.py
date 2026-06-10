e0c1da59b169ff25f993a5493e4822afb7b8a6b8
Henrik Wahlström <henrik.g.wahlstrom@pm.me>
2026-06-10 22:33:51 +0200

    Save ids and events to file.
    Event ids and parsed event details are now save to file. Event IDs are parsed from the schedule and saved to file, grouped by date and time. The IDs from this file are used to get further event details, which are save to another file.
    
    Also added timeouts to GET requests, with multiple retries for gettign event details.

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+from collections.abc import Iterable, Iterator
+import datetime
+from html import unescape
+import json
+from pathlib import Path
+
 from bs4 import BeautifulSoup
 import requests
 
+TIMEOUT_SECONDS = 3
+NUM_RETRIES = 3
 
-def main():
+
+def get_ids(url: str, verbose=False) -> dict:
+    dates = {}
+
     # Get page.
-    response = requests.get('https://www.medeltidsveckan.se/programme/')
+    response = requests.get(url, timeout=TIMEOUT_SECONDS)
     response.raise_for_status()
     soup = BeautifulSoup(response.content, 'html.parser')
 
@@ -13,36 +24,137 @@ def main():
 
     # Get days.
     days = soup.select('.day')
-    for day_index, day in enumerate(days):
-        date_string = day.get('id').removeprefix('date-')
-        weekday = weekdays[day_index]
-        print(weekday, date_string)
+    for day_index, day_element in enumerate(days):
+        times = {}
+        date_string = day_element.get('id').removeprefix('date-')
+        if verbose:
+            weekday = weekdays[day_index]
+            print(weekday, date_string)
 
         # Get time slots.
-        time_views = day.select('.time-view')
+        time_views = day_element.select('.time-view')
         for time_view in time_views:
-            time = time_view.select_one('h4')
-            print(time.string)
+            ids = []
+            time_string = time_view.select_one('h4').string
+            if verbose:
+                print('-', time_string)
 
             # Get events.
             for event in time_view.select('article'):
-                title = event.select_one('strong').string
                 event_id = int(event.get('data-pid'))
+                if verbose:
+                    print('  -', event_id)
+                ids.append(event_id)
 
-                # Get details.
-                response = requests.get(
-                    'https://www.medeltidsveckan.se',
-                    params={
-                        'action': 'fetch-programme-item',
-                        'pid': event_id
-                    }
-                )
-                response.raise_for_status()
-                details = response.json()
-                venue = details['sidebar']['venue']
-                print(f'- [{event_id}] {title} ({venue})')
+            times[time_string] = ids
+        dates[date_string] = times
+    return dates
 
-        print()
+
+def get_event(url: str, event_id: int, quiet=False, verbose=False) -> dict:
+    # Get json.
+    response = requests.get(
+        url,
+        params={
+            'action': 'fetch-programme-item',
+            'pid': event_id
+        },
+        timeout=TIMEOUT_SECONDS
+    )
+    response.raise_for_status()
+    details = response.json()
+
+    # Parse event details.
+    event = {}
+    if image_url := details.get('image'):
+        event['image_url'] = image_url
+    if item_owner := details['header']['item_owner']:
+        event['owner'] = unescape(item_owner)
+    event['title'] = unescape(details['header']['title'])
+    event['html_escaped_description'] = details['content']['description']
+    if siblings := details['content']['siblings']:
+        event['siblings'] = []
+        for sibling in siblings.values():
+            timestamp = int(sibling['timestamp'])
+            date = datetime.date.fromtimestamp(timestamp)
+            event['siblings'].append({
+                'date': str(date),
+                'weekday': unescape(sibling['day']),
+                'start_time': unescape(sibling['time']),
+                'title': unescape(sibling['title']),
+            })
+    event['weekday'] = details['sidebar']['dayName']
+    event['date'] = details['sidebar']['date']
+    start, end = details['sidebar']['time'].split(' - ')
+    event['start_time'] = unescape(start)
+    event['end_time'] = unescape(end)
+    event['venue'] = unescape(details['sidebar']['venue'])
+    if ticket_url := details['sidebar']['ticket_link']:
+        event['ticket_url'] = ticket_url
+    if not quiet:
+        if verbose:
+            print(json.dumps(event, indent=4, ensure_ascii=False))
+        else:
+            print('-', event_id, event['weekday'], event['start_time'], event['title'])
+
+    # DEBUG validation.
+    for k, v in event.items():
+        if v is None:
+            print(json.dumps(event, indent=4, ensure_ascii=False))
+            raise RuntimeError(f'{k} in event {event_id} is None')
+        if v == "":
+            print(json.dumps(event, indent=4, ensure_ascii=False))
+            raise RuntimeError(f'{k} in event {event_id} is empty')
+
+    return event
+
+
+def main():
+    program_url = 'https://www.medeltidsveckan.se/programme/'
+    details_url = 'https://www.medeltidsveckan.se/'
+    ids_filepath = Path(__file__).parent / 'event_ids.json'
+    events_filepath = Path(__file__).parent / 'events_v1.json'
+
+    # Parse and save event IDs.
+    print('Gettings IDs')
+    event_ids: list[int] | None = None
+    try:
+        event_ids = get_ids(program_url)
+        with open(ids_filepath, 'w') as file:
+            json.dump(event_ids, file, indent=4)
+        print('IDs saved.')
+    except requests.exceptions.ConnectTimeout:
+        print('Timeout gettings event IDs. Skipping to next step.')
+
+    # Get and save events.
+    print('Gettings events')
+    with open(ids_filepath, 'r') as file:
+        event_ids_by_date = json.load(file)
+        event_ids: list[int] = []
+        for times in event_ids_by_date.values():
+            for ids in times.values():
+                event_ids.extend(ids)
+    print('IDs loaded from file.')
+
+    events = {}
+    for event_id in event_ids:
+        num_failed_attempts = 0
+        event = None
+        while event is None and num_failed_attempts < NUM_RETRIES:
+            try:
+                event = get_event(details_url, event_id)
+            except requests.exceptions.ReadTimeout:
+                num_failed_attempts += 1
+                print(f'Attempt {num_failed_attempts}/{NUM_RETRIES} to get info for event {event_id} timeout.')
+            break
+        if event is None:
+            print(f'Failed to parse event {event_id}.')
+            continue
+        events[event_id] = event
+
+    with open(events_filepath, 'w', encoding='utf-8') as file:
+        json.dump(events, file, ensure_ascii=False, indent=4)
+    print(f'Events saved.')
 
 
 if __name__ == "__main__":
